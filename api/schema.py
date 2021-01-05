3c93d75a4f7a3136ac3bf476b9473d0d52e0fe8c
MaxMhonc <mhonc@softserveinc.com>
2021-01-05 13:39:55 +0200

    updated .flake8

--- api/schema.py
+++ api/schema.py
@@ -1,58 +1,9 @@
-from functools import partial
-
 from marshmallow import ValidationError, Schema, fields, INCLUDE
 
 
 def validate_string(value):
     if value == '':
         raise ValidationError('Field may not be blank.')
-
-    # if choices is not None:
-    #     if value not in choices:
-    #         raise ValidationError(
-    #             f'Must be one of: {", ".join(map(repr, choices))}.'
-    #         )
-
-
-# OBSERVABLE_TYPE_CHOICES = (
-#     'amp_computer_guid',
-#     'certificate_common_name',
-#     'certificate_issuer',
-#     'certificate_serial',
-#     'cisco_mid',
-#     'device',
-#     'domain',
-#     'email',
-#     'email_messageid',
-#     'email_subject',
-#     'file_name',
-#     'file_path',
-#     'hostname',
-#     'imei',
-#     'imsi',
-#     'ip',
-#     'ipv6',
-#     'mac_address',
-#     'md5',
-#     'ms_machine_id',
-#     'mutex',
-#     'ngfw_id',
-#     'ngfw_name',
-#     'odns_identity',
-#     'odns_identity_label',
-#     'orbital_node_id',
-#     'pki_serial',
-#     'process_name',
-#     'registry_key',
-#     'registry_name',
-#     'registry_path',
-#     's1_agent_id',
-#     'sha1',
-#     'sha256',
-#     'url',
-#     'user',
-#     'user_agent'
-# )
 
 
 class ObservableSchema(Schema):
